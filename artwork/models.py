1cdbd01fb2d1934e048026e5b656d2577f29fd6a
riyuzakitakeda <rpgfloor@gmail.com>
2019-11-21 11:55:25 +0800

    fix all

--- artwork/models.py
+++ artwork/models.py
@@ -5,6 +5,8 @@ class Artwork(models.Model):
     judul = models.CharField(max_length=100)
     video_url = models.CharField(max_length=1000)
     image_url = models.CharField(max_length=1000)
+    image2_url = models.CharField(max_length=1000)
+    image3_url = models.CharField(max_length=1000)
     deskripsi = models.TextField(max_length=2000)
 
 
